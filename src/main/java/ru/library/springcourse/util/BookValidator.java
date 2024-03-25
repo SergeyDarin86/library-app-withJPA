7e32fee311972804223cb469742def84cf137dee
sergei_darin <swd86@mail.ru>
2024-03-25 22:35:56 +0400

    Логгирование для BookDAO

--- src/main/java/ru/library/springcourse/util/BookValidator.java
+++ src/main/java/ru/library/springcourse/util/BookValidator.java
@@ -1,5 +1,6 @@
 package ru.library.springcourse.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -7,6 +8,7 @@ import org.springframework.validation.Validator;
 import ru.library.springcourse.dao.BookDAO;
 import ru.library.springcourse.models.Book;
 
+@Slf4j
 @Component
 public class BookValidator implements Validator {
 
@@ -25,6 +27,8 @@ public class BookValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         Book book = (Book) target;
+
+        log.info("Start method validate for Book, bookTitle is: " + book.getTitle());
 
         if (bookDAO.show(book.getTitle()).isPresent()) {
             if (bookDAO.show(book.getTitle()).get().getBookId() != book.getBookId()){
